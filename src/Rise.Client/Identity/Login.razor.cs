442b8bfdfd420ba3e0d5fc5c1295ddf044c65b57
Jasper Van Zeir <49187306+R3D-R4V3N@users.noreply.github.com>
2025-10-14 16:27:20 +0200

    Redirect login to homepage and add user info

--- src/Rise.Client/Identity/Login.razor.cs
+++ src/Rise.Client/Identity/Login.razor.cs
@@ -1,5 +1,8 @@
+using Ardalis.Result;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Rise.Shared.Identity.Accounts;
+using System.Threading.Tasks;
 
 namespace Rise.Client.Identity;
 
@@ -9,15 +12,51 @@ public partial class Login
 
     private AccountRequest.Login Model = new();
     private Result _result = new();
+    private bool _hasNavigated;
+
     [Inject] public required IAccountManager AccountManager { get; set; }
     [Inject] public required NavigationManager Navigation { get; set; }
+
+    [CascadingParameter]
+    private Task<AuthenticationState>? AuthenticationStateTask { get; set; }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        await RedirectAuthenticatedUserAsync();
+    }
+
     public async Task LoginUser()
     {
         _result = await AccountManager.LoginAsync(Model.Email!, Model.Password!);
 
-        if (_result.IsSuccess && !string.IsNullOrEmpty(ReturnUrl))
+        if (!_result.IsSuccess)
         {
-            Navigation.NavigateTo(ReturnUrl);
+            return;
+        }
+
+        _hasNavigated = true;
+
+        if (!string.IsNullOrEmpty(ReturnUrl))
+        {
+            Navigation.NavigateTo(ReturnUrl, forceLoad: true);
+            return;
+        }
+
+        Navigation.NavigateTo("/homepage", forceLoad: true);
+    }
+
+    private async Task RedirectAuthenticatedUserAsync()
+    {
+        if (_hasNavigated || AuthenticationStateTask is null)
+        {
+            return;
+        }
+
+        var authState = await AuthenticationStateTask;
+        if (authState.User.Identity?.IsAuthenticated == true)
+        {
+            _hasNavigated = true;
+            Navigation.NavigateTo("/homepage", forceLoad: true);
         }
     }
 }
