f7cba331359b5093fe8a22cb14c8c4f7d2c1d5ce
Jasper Van Zeir <49187306+R3D-R4V3N@users.noreply.github.com>
2025-10-14 23:08:12 +0200

    Use shared default profile avatar

---
+++ src/Rise.Shared/Assets/DefaultImages.cs
@@ -0,0 +1,6 @@
+namespace Rise.Shared.Assets;
+
+public static class DefaultImages
+{
+    public const string Profile = "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?auto=format&fit=facearea&facepad=2.5&w=200&h=200&q=80";
+}
